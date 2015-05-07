7c979b820145d78a72fec4c99263fcdcefa31aec
Rik van den Eijnden <rik@vdeijnden.com>
2015-05-07 14:54:19 +0200

    Decrease the amount of ram
    RAM set to 512kb

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom bizim.iso -curses -m 1560
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom bizim.iso -curses -m 512
