4ffa47955878cb1fa7f60915ba4e985f7b961675
Rik van den Eijnden <rik@vdeijnden.com>
2015-05-14 12:12:40 +0200

    Add -display option on command line.
    No longer using old -curses

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom rho.iso -curses -m 512
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom rho.iso -m 512 -display curses
