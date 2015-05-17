43b252bfa246ae2dd244d75fe0076d363ae4c4b9
Rik van den Eijnden <rik@vdeijnden.com>
2015-05-17 09:18:00 +0200

    Curly braces added in order ro hush up the compiler

--- kernel/arch/i386/timer.c
+++ kernel/arch/i386/timer.c
@@ -37,8 +37,9 @@ static void timer_phase(int hz) {
  */
 void timer_handler (struct regs *r) {
 	timer_ticks++;
-	if (timer_ticks % _clockrate == 0)		/* one second has passed */
+	if (timer_ticks % _clockrate == 0) {		/* one second has passed */
 		;
+	}
 }
 
 /*
