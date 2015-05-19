7fe981d4ae28ea07af05b423d795767e111f5fed
reijnden <reijnden@users.noreply.github.com>
2015-05-19 13:10:13 +0200

    Documentation
    Function descriptions added

--- kernel/arch/i386/iobus.c
+++ kernel/arch/i386/iobus.c
@@ -1,10 +1,16 @@
 
+/*
+ * Read an unsigned char from given IO channel
+ */
 unsigned char inportb(unsigned short _port) {
 	unsigned char c;
 	__asm__ __volatile__ ("inb %1, %0" : "=a" (c) : "dN" (_port));
 	return c;
 }
 
+/*
+ * Write an unsigned char to given IO channel
+ */
 void outportb(unsigned short _port, unsigned char _data) {
 	__asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
