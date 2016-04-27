40400b26304f3ebee127a6b600ed4c3f0e881d39
Rik van den Eijnden <rvdeijnden@gmail.com>
2016-04-27 16:52:43 +0200

    Differentiate between kernel and userspace calls

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #if defined(__is_rho_kernel)
 #include <kernel/tty.h>
 #endif
 
+/*
+ * putchar writes the character c, cast to an unsigned char, to stdout
+ */
 int putchar(int ic)
 {
 #if defined(__is_rho_kernel)
-	char c = (char) ic;
+	char c = (unsigned char) ic;
 	terminal_write(&c, sizeof(c));
 #else
 	// TODO: You need to implement a write system call.
+	abort();
 #endif
 	return ic;
 }
