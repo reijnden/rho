bff95709c455596e813697191fbc4b0a4e491e0e
Rik van den Eijnden <rvdeijnden@gmail.com>
2016-04-27 17:08:12 +0200

    Differentiate between kernel and userspace calls

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,10 +1,17 @@
 #include <string.h>
+#include <stdlib.h>
 
 void* memcpy(void* __restrict dstptr, const void* __restrict srcptr, size_t size)
 {
+#if defined(__is_rho_kernel)
 	unsigned char* dst = (unsigned char*) dstptr;
 	const unsigned char* src = (const unsigned char*) srcptr;
 	for ( size_t i = 0; i < size; i++ )
 		dst[i] = src[i];
 	return dstptr;
+#else
+	//TODO
+	abort();
+	return 0;
+#endif
 }
