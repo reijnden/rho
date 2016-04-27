529b55858293fc675b010db9e2556b5f60c521d7
Rik van den Eijnden <rvdeijnden@gmail.com>
2016-04-27 17:09:23 +0200

    Differentiate between kernel and userspace calls

--- libc/string/memset.c
+++ libc/string/memset.c
@@ -1,9 +1,16 @@
 #include <string.h>
+#include <stdlib.h>
 
 void* memset(void* bufptr, int value, size_t size)
 {
+#if defined(__is_rho_kernel)
 	unsigned char* buf = (unsigned char*) bufptr;
 	for ( size_t i = 0; i < size; i++ )
 		buf[i] = (unsigned char) value;
 	return bufptr;
+#else
+	//TODO
+	abort();
+	return 0;
+#endif
 }
