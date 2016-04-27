cdb6a5c4915b62f0e4c37a1fa2185263768e8dfc
Rik van den Eijnden <rvdeijnden@gmail.com>
2016-04-27 17:08:36 +0200

    Differentiate between kernel and userspace calls

--- libc/string/memcmp.c
+++ libc/string/memcmp.c
@@ -1,7 +1,9 @@
 #include <string.h>
+#include <stdlib.h>
 
 int memcmp(const void* aptr, const void* bptr, size_t size)
 {
+#if defined(__is_rho_kernel)
 	const unsigned char* a = (const unsigned char*) aptr;
 	const unsigned char* b = (const unsigned char*) bptr;
 	for ( size_t i = 0; i < size; i++ )
@@ -10,4 +12,9 @@ int memcmp(const void* aptr, const void* bptr, size_t size)
 		else if ( b[i] < a[i] )
 			return 1;
 	return 0;
+#else
+	// TODO
+	abort();
+	return 0;
+#endif
 }
