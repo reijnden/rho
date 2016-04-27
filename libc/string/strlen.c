780787f9363d3a13693823c4e48f3e4f1b873d79
Rik van den Eijnden <rvdeijnden@gmail.com>
2016-04-27 17:09:10 +0200

    Differentiate between kernel and userspace calls

--- libc/string/strlen.c
+++ libc/string/strlen.c
@@ -1,9 +1,16 @@
 #include <string.h>
+#include <stdlib.h>
 
 size_t strlen(const char* string)
 {
+#if defined(__is_rho_kernel)
 	size_t result = 0;
 	while ( string[result] )
 		result++;
 	return result;
+#else
+	//TODO
+	abort();
+	return -1;
+#endif
 }
